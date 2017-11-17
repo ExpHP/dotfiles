aa175f380cfee776a7d7dfb418ce64eb9f31b132
Michael Lamparski <diagonaldevice@gmail.com>
2017-11-17 11:47:33 -0500

    I should commit more often

---
+++ gdbinit
@@ -0,0 +1,5 @@
+set history filename ~/.gdb_history
+set history save on
+set history size 10000000
+set history expansion on
+show history
--- python-startup.py
+++ python-startup.py
@@ -6,3 +6,30 @@ def history(file=___stdout):
     import readline
     for i in range(readline.get_current_history_length()):
         print(str(readline.get_history_item(i)), file=file)
+
+# Jesus H. Christ why haven't I done this sooner
+#   - Me, November 2017
+#
+# (even if these aren't all that useful to use on the command line,
+#  I might still want them around just to check docstrings)
+import os
+import sys
+import json
+import time
+import shutil
+import subprocess
+import argparse
+import math
+import numpy
+np = numpy
+try: import yaml
+except: pass
+
+def pmg():
+    global Poscar
+    global Structure
+    global Lattice
+    global pymatgen
+    import pymatgen
+    from pymatgen import Structure, Lattice
+    from pymatgen.io.vasp import Poscar
