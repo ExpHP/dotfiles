c3217f802d35d4dc4f8713e6c7f84fd1f5bbc86e
Michael Lamparski <diagonaldevice@gmail.com>
2023-09-25 13:21:08 -0400

    Use gittool-probe.py in prompt

--- shell/gittool/gittool-probe.py
+++ shell/gittool/gittool-probe.py
@@ -12,6 +12,25 @@ import typing as tp
 PROG = os.path.basename(sys.argv[0])
 ENV_PREFIX = 'EXPHP_GITTOOL'
 
+# This script is meant to have its STDOUT sourced by bash. e.g.
+#
+#    . <($HOME/dotfiles/shell/gittool/gittool-probe.py)
+#
+# The text it writes to STDOUT sets the following environment variables:
+#
+# These are "stringly booleans."
+# These are either an empty string (false) or something else (true).
+# Use the syntax [[ $VAR_NAME ]] to test them.
+#   EXPHP_GITTOOL_BRANCH   : branch name (or "HEAD" when detached). If empty: not in a repo.
+#   EXPHP_GITTOOL_IS_BARE  : (contents are unspecified beyond truthiness)
+#   EXPHP_GITTOOL_IS_DIRTY : (contents are unspecified beyond truthiness)
+#   EXPHP_GITTOOL_ERROR    : contents is an error message
+#
+# These are integers:
+#   EXPHP_GITTOOL_UNTRACKED : untracked file count
+#   EXPHP_GITTOOL_ADDITIONS : # inserted lines
+#   EXPHP_GITTOOL_DELETIONS : # deleted lines
+
 def main():
     parser = argparse.ArgumentParser(
         description='Probe git repository for status',
@@ -51,6 +70,7 @@ def main_impl(probe_dir: Path) -> "SuccessOutput":
     return SuccessOutput(
         branch=branch,
         is_bare=is_bare,
+        is_dirty=stats.is_dirty,
         add_count=stats.add_count,
         delete_count=stats.delete_count,
         untracked_count=stats.untracked_count,
@@ -94,17 +114,18 @@ class GitCommands:
         return bool_from_git(stdout.strip())
 
     def count_stats(self) -> "GitStats":
-        stdout = self.git_stdout(['status', '--porcelain'])
+        is_dirty = False
         untracked_count = 0
+        stdout = self.git_stdout(['status', '--porcelain']).strip()
         for line in stdout.splitlines():
+            is_dirty = True
             if line.startswith('?? '):
                 untracked_count += 1
 
-        stdout = self.git_stdout(['diff', '--shortstat']).strip()
-        stdout_words = stdout.split()
         add_count = 0
         delete_count = 0
-
+        stdout = self.git_stdout(['diff', '--shortstat']).strip()
+        stdout_words = stdout.split()
         while stdout_words:
             # Output looks like: (with each part optional)
             #    1 file changed, 1 insertion(+), 2 deletions(-)
@@ -121,11 +142,12 @@ class GitCommands:
 
         return GitStats(
             untracked_count=untracked_count,
+            is_dirty=is_dirty,
             add_count=add_count,
             delete_count=delete_count,
         )
 
-GitStats = namedtuple('GitStatus', ["add_count", "delete_count", "untracked_count"])
+GitStats = namedtuple('GitStatus', ["add_count", "delete_count", "untracked_count", "is_dirty"])
 
 def bool_from_git(gitbool: str):
     if gitbool == 'true':
@@ -151,12 +173,14 @@ class SuccessOutput:
             self,
             branch: tp.Optional[str],
             is_bare: bool,
+            is_dirty: bool,
             add_count: int,
             delete_count: int,
             untracked_count: int,
     ):
         self.branch = branch
         self.is_bare = is_bare
+        self.is_dirty = is_dirty
         self.add_count = add_count
         self.delete_count = delete_count
         self.untracked_count = untracked_count
@@ -166,6 +190,7 @@ class SuccessOutput:
         return cls(
             branch=None,
             is_bare=False,
+            is_dirty=False,
             add_count=0,
             delete_count=0,
             untracked_count=0
@@ -176,6 +201,7 @@ class SuccessOutput:
         return cls(
             branch=branch,
             is_bare=True,
+            is_dirty=False,
             add_count=0,
             delete_count=0,
             untracked_count=0
@@ -185,9 +211,10 @@ class SuccessOutput:
         return "\n".join([
             "{}_BRANCH={}".format(ENV_PREFIX, shlex.quote(self.branch)),
             "{}_IS_BARE={}".format(ENV_PREFIX, bool_to_bash(self.is_bare)),
-            "{}_ADD_COUNT={}".format(ENV_PREFIX, self.add_count),
-            "{}_DELETE_COUNT={}".format(ENV_PREFIX, self.delete_count),
-            "{}_UNTRACKED_COUNT={}".format(ENV_PREFIX, self.untracked_count),
+            "{}_IS_DIRTY={}".format(ENV_PREFIX, bool_to_bash(self.is_dirty)),
+            "{}_ADDITIONS={}".format(ENV_PREFIX, self.add_count),
+            "{}_DELETIONS={}".format(ENV_PREFIX, self.delete_count),
+            "{}_UNTRACKED={}".format(ENV_PREFIX, self.untracked_count),
         ]) + "\n"
 
 def bool_to_bash(b: bool):
